6e3f38398585a9d2cc93d2876a74e6e88442e1c2
Phsithzz <phoosith2548@gmail.com>
2025-11-13 14:25:35 +0700

    Sign In Design

--- lib/row_example.dart
+++ lib/row_example.dart
@@ -8,25 +8,98 @@ class RowExample extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+
         appBar: AppBar(
-          title: const Text("Row Example"),
+
+          title: const Text(
+
+            "Row Example",
+            style: TextStyle(fontWeight: FontWeight.bold),
+            
+          ),
+
           backgroundColor: Colors.lightBlueAccent,
           foregroundColor: Colors.white,
-          ),
-        body: Container(
-          padding:EdgeInsets.fromLTRB(0, 10, 0, 10),
-          child:const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child:Text("Sign in to My App"),
-            ),
-            Text("User name"),
-            SizedBox(height:20),
-            Text("Password"),
-          ],
+
         ),
-        )
+        body: Container(
+          // padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+          margin: EdgeInsets.all(10),
+          child: Column(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+
+              const Center(
+                child: Text(
+                  "Sign in to My App",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              const Text(
+                "User name",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+
+              const TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  prefixIcon: Icon(Icons.person),
+                ),
+
+                style: TextStyle(color: Colors.lightBlueAccent),
+              ),
+
+              const SizedBox(height: 20),
+
+              const Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              ElevatedButton.icon(
+                icon: const Icon(Icons.lock),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.only(
+                    left: 25,
+                    right: 25,
+                    top: 20,
+                    bottom: 20,
+                  ),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                label: const Text("Sign In"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
